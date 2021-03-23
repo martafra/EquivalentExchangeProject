f780c5e506d5691a67a87c923999832cf4a29ee3
Marta Fraioli <58730455+martafra@users.noreply.github.com>
2021-03-23 21:52:56 +0100

    Marta - aggiunto attributo "genre" all'entity Book

--- src/logic/entity/Book.java
+++ src/logic/entity/Book.java
@@ -1,10 +1,11 @@
 package logic.entity;
+import logic.enumeration.BookGenre;
 
 public class Book {
 	private String author;
 	private Integer edition;
 	private Integer numberOfPages;
-	////private enum bookGenre {NEW, PRATICALLYNEW,VERYGOOD, GOOD, ACCEPTABLE }
+	private BookGenre genre;
 	private String  publishingHouse;
 	
 	public String getAuthor() {
@@ -17,6 +18,10 @@ public class Book {
 	
 	public Integer getNumberOfPages() {
 		return this.numberOfPages;
+	}
+	
+	public BookGenre getGenre() {
+		return this.genre;
 	}
 	
 	public String getPublishingHouse() {
@@ -33,6 +38,10 @@ public class Book {
 	
 	public void setNumberOfPages(Integer numberOfPages) {
 		this.numberOfPages = numberOfPages;
+	}
+	
+	public void setGenre(BookGenre genre) {
+		this.genre = genre;
 	}
 	
 	public void setPublishingHouse(String publishingHouse) {
