a6046fcbc39196081d6cd31a3324e8bd70770b45
Marta Fraioli <58730455+martafra@users.noreply.github.com>
2021-04-19 16:42:08 +0200

    Marta - aggiunta entity "Request"

---
+++ src/logic/entity/Request.java
@@ -0,0 +1,41 @@
+package logic.entity;
+
+public class Request {
+
+	private User buyer;
+	private ItemInSale referedItem;
+	private Boolean requestStatus;
+	private String note;
+
+	public User getBuyer() {
+		return this.buyer;
+	}
+	public User getSeller() {
+		return this.referedItem.getSeller();
+	}
+	public ItemInSale getReferedItem() {
+		return this.referedItem;
+	}
+	public Boolean getStatus() {
+		return this.requestStatus;
+	}
+	public String getNote() {
+		return this.note;
+	}
+
+	public void setBuyer(User buyer) {
+		this.buyer = buyer;
+	}
+	public void setReferedItem(ItemInSale item) {
+		this.referedItem = item;
+	}
+	public void setStatus(Boolean status) {
+		this.requestStatus = status;
+	}
+	public void setNote(String note) { // inserire controllo sul limite di caratteri
+		this.note = note;
+	}
+
+
+
+}
